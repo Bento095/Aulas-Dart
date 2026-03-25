eed90349efd90e49179654f787e00c4b7e7320d1
Bruno Bento <brunoffbento@gmail.com>
2026-03-24 21:42:02 -0300

    Continuando aula

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,7 +1,26 @@
 import 'dart:io';
 
 void main() {
-  var numeroUm = stdin.readLineSync(); // captura a entrada do usuário
-  var numeroDois = stdin.readLineSync(); // captura a entrada do usuário
-  print(numeroUm! + numeroDois!); // concatena as duas entradas
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+  
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+
+  }
+
+
+  multiplicacao();
 }
