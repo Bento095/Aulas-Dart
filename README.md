4299a56c3f1e735d0f0724573a81a8223a45a0e6
Bruno Bento <brunoffbento@gmail.com>
2026-03-31 23:49:48 -0300

    Implementação do bloco if e explicações sobre switch case.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+🧮 Projeto: Calculadora Dart (Aula Guiada Da ALURA)
+Este repositório contém o código desenvolvido durante a aula prática, seguindo o passo a passo do professor para entender a estrutura de um app CLI (Command Line Interface).
+
+🚀 O que foi construído:
+Entrada de Dados: Captura de números e da operação matemática via stdin.
+
+Estrutura de Controle: Implementação de if / else (ou switch) para selecionar a operação.
+
+Funções Matemáticas: Separação da lógica de soma, subtração, multiplicação e divisão.
+
+Tratamento de Erros: Como evitar que o programa quebre ao digitar letras ou dividir por zero.
+
+🧠 Aprendizados da Aula:
+Organização de Código: Como o professor estrutura as funções fora do main.
+
+Tipagem de Retorno: Prática de funções que retornam double para cálculos precisos.
+
+Fluxo de Execução: Entendimento de como o Dart lê o código de cima para baixo.
+
+🛠️ Tecnologias:
+Dart SDK
+
+Biblioteca dart:io
--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,9 +1,16 @@
+// imports
 import 'dart:io';
 
+//corpo principal do programa.
 void main() {
+  print('Digite o primeiro número: ');
   double numeroUm = double.parse(stdin.readLineSync()!);
+  print('Escolha a operação (+ - / *): ');
+  String operacao = stdin.readLineSync()!;
+  print('Digite o segundo número: ');
   double numeroDois = double.parse(stdin.readLineSync()!);
 
+// bloco de definir as operações por função.
   void soma() {
     print(numeroUm + numeroDois);
   }
@@ -18,9 +25,38 @@ void main() {
 
   void multiplicacao() {
     print(numeroUm * numeroDois);
-
   }
 
 
-  multiplicacao();
+  //bloco das operações por if/else.
+  if (operacao == '+') {
+    soma();
+  } else{
+    if (operacao == '-') {
+        subtracao();
+      } else {
+        if (operacao == '/') {
+            divisao();
+          } else {
+            if (operacao == '*') {
+            multiplicacao();
+        }
+      }
+    }
+  }
+
+  switch (operacao) {
+    case '+':
+    soma();
+
+    case '-':
+    subtracao();
+
+    case '/':
+    divisao();
+
+    case '*':
+    multiplicacao();
+    break;
+  }
 }
